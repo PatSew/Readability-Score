ac4168057c254f9fe58487150454b28fd825cd9c
PatSew <seweryn.patk@gmail.com>
2020-12-09 23:20:41 +0100

    before merging second stage

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -5,7 +5,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input.length() > 100 ? "HARD" : "EASY");
+        String[] input = scanner.nextLine().split("[.!?]");
+        System.out.println(retCoun(input) > 10 ? "HARD" : "EASY");
+    }
+
+    public static int retCoun(String[] arr) {
+        int sum = 0;
+        int count = 0;
+        int result;
+        for (String w : arr) {
+            String[] s = w.split(" ");
+            sum += s.length;
+            count++;
+        }
+        result = sum / count;
+        return result;
     }
 }
